089fe2d36b127b05916f070555f2108627c42b68
Tessa Power <tessa@tessapower.co>
2022-03-31 11:14:13 +1300

    Set color properties on TShape objects to public

--- graphics/TShape.java
+++ graphics/TShape.java
@@ -5,9 +5,9 @@ import engine.GraphicsCtx;
 import java.awt.*;
 
 abstract public class TShape extends TObject {
-    Color outlineColor;
-    Color fillColor;
-    boolean isFilled;
+    public Color outlineColor;
+    public Color fillColor;
+    public boolean isFilled;
 
     public TShape(Dimension dimension) {
         super(dimension);
