5b722875dd4366fd980be0edd9dbae141cd206cc
Tessa Power <tessa@tessapower.co>
2022-04-04 12:59:41 +1200

    Initial support for physics and collisions

---
+++ physics/collisions/CollisionPrimitive.java
@@ -0,0 +1,11 @@
+package physics.collisions;
+
+import java.awt.*;
+
+public interface CollisionPrimitive {
+    boolean contains(Point p);
+
+    boolean collides(CollisionPrimitive bounds);
+
+    void setOrigin(Point point);
+}
