e41e6817a7d85e0d910c5d4ac9ab056d8faed486
Tessa Power <tessa@tessapower.co>
2022-04-13 15:16:12 +1200

    Implement initial version of CollisionEvent class

--- collisions/CollisionEvent.java
+++ collisions/CollisionEvent.java
@@ -1,5 +1,42 @@
 package collisions;
 
-public class CollisionEvent {
+import actors.Actor;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CollisionEvent {
+    private final Actor movingActor;
+    private final Set<Actor> staticActors;
+    private Vector collisionVector;
+
+    public CollisionEvent(Actor movingActor, Actor staticActor) {
+        this.collisionVector = null;
+        this.movingActor = movingActor;
+        staticActors = new HashSet<>();
+        add(staticActor);
+    }
+
+    public void add(Actor... staticActors) {
+        this.staticActors.addAll(List.of(staticActors));
+        // calculate distance between moving actor origin and static actor origins
+    }
+
+    public Actor movingActor() {
+        return movingActor;
+    }
+
+    public Set<Actor> staticActors() {
+        return Collections.unmodifiableSet(staticActors);
+    }
+
+    public void setCollisionVector(Vector collisionVector) {
+        this.collisionVector = collisionVector;
+    }
+
+    public Vector collisionVector() {
+        return collisionVector;
+    }
 }
