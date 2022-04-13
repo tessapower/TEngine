4dcbc788e5c86b239e9d2b6e64f486e65dba0452
Tessa Power <tessa@tessapower.co>
2022-04-13 15:24:11 +1200

    Implement initial PhysicsEngine without resolving collisions

---
+++ physics/kinematics/PhysicsEngine.java
@@ -0,0 +1,67 @@
+package physics.kinematics;
+
+import actors.Actor;
+import collisions.CollisionDetector;
+import collisions.CollisionEvent;
+import collisions.CollisionEventNotifier;
+
+import java.awt.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PhysicsEngine {
+    CollisionDetector collisionDetector;
+    Set<Actor> actors;
+    CollisionEventNotifier collisionEventNotifier;
+
+    public PhysicsEngine() {
+        collisionDetector = new CollisionDetector();
+        actors = new HashSet<>();
+    }
+
+    public void update() {
+        // Step 1: Move everything that can move
+        for (var actor : actors) {
+            if (actor.physicsProps().movable) {
+                Point origin = actor.origin();
+                Velocity velocity = actor.physicsProps().velocity;
+                origin.translate(velocity.dx(), velocity.dy());
+                actor.setOrigin(origin);
+            }
+        }
+
+        // Step 2: Detect collisions
+         Collection<CollisionEvent> collisions = collisionDetector.detectCollisions(actors);
+
+        // Step 3: Resolve collisions
+        // resolveCollisions(collisions);
+
+        // Step 4: Notify collisions
+        if (collisionEventNotifier != null) {
+             collisions.forEach(event -> collisionEventNotifier.notifyCollision(event));
+        }
+    }
+
+    public void setCollisionEventNotifier(CollisionEventNotifier eventNotifier) {
+        collisionEventNotifier = eventNotifier;
+    }
+
+    public void add(Actor actor) {
+        actors.add(actor);
+    }
+
+    public void addAll(Actor... actors) {
+        for (var actor : actors) {
+            add(actor);
+        }
+    }
+
+    public void remove(Actor actor) {
+        actors.remove(actor);
+    }
+
+    public void removeAll() {
+        actors.clear();
+    }
+}
