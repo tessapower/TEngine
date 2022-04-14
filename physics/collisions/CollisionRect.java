6eb06a3c6419696e7435d74950aa01aa4db67a96
Tessa Power <tessa@tessapower.co>
2022-04-14 22:54:53 +1200

    Add center(), origin(), dimension() to CollisionShape interface

--- physics/collisions/CollisionRect.java
+++ physics/collisions/CollisionRect.java
@@ -3,8 +3,8 @@ package physics.collisions;
 import java.awt.*;
 
 public class CollisionRect implements CollisionShape {
-    Point origin;
-    Dimension dimension;
+    protected Point origin;
+    protected Dimension dimension;
 
     public CollisionRect(Point origin, Dimension dimension) {
         this.origin = origin;
@@ -12,7 +12,30 @@ public class CollisionRect implements CollisionShape {
     }
 
     @Override
+    public Dimension dimension() {
+        return dimension;
+    }
+
+    @Override
+    public Point origin() {
+        return origin;
+    }
+
+    @Override
     public void setOrigin(Point origin) {
         this.origin = origin;
+    }
+
+    @Override
+    public boolean contains(Point point) {
+        return point.x >= origin.x
+                && point.y >= origin.y
+                && point.x < origin.x + dimension.width
+                && point.y < origin.y + dimension.height;
+    }
+
+    @Override
+    public Point center() {
+        return new Point((origin.x + dimension.width / 2), (origin.y + dimension.height / 2));
     }
 }
--- physics/collisions/CollisionShape.java
+++ physics/collisions/CollisionShape.java
@@ -3,5 +3,11 @@ package physics.collisions;
 import java.awt.*;
 
 public interface CollisionShape {
+    Dimension dimension();
+    Point origin();
     void setOrigin(Point origin);
+    boolean contains(Point point);
+    Point center();
+    boolean equals(Object o);
+    int hashCode();
 }
