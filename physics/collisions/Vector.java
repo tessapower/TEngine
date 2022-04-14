812b58ac902949415f7477400426244ead0ecd00
Tessa Power <tessa@tessapower.co>
2022-04-14 22:56:00 +1200

    Implement magnitude() and add normalize() methods to Vector

--- physics/collisions/Vector.java
+++ physics/collisions/Vector.java
@@ -1,7 +1,16 @@
 package physics.collisions;
 
-public record Vector(int x, int y) {
+public record Vector(double x, double y) {
+
     public Vector() {
         this(0, 0);
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vector normalize() {
+        return new Vector((x / magnitude()), (y / magnitude()));
     }
 }
