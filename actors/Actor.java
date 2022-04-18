f56061ef416b93a9e175de32b405428c0ec5de2c
Tessa Power <tessa@tessapower.co>
2022-04-18 13:12:24 +1200

    Rename sprite in Actor to graphicObject so we can implement Sprite class!

--- actors/Actor.java
+++ actors/Actor.java
@@ -11,21 +11,21 @@ import java.util.Objects;
 public abstract class Actor {
     protected Point origin;
     protected PhysicsBody physicsBody;
-    protected TGraphicObject sprite;
+    protected TGraphicObject graphicObject;
     protected World world;
 
     public Actor() {
         origin = new Point();
         physicsBody = new PhysicsBody();
-        sprite = null;
+        graphicObject = null;
         world = null;
     }
 
     public void setOrigin(Point origin) {
         this.origin = origin;
 
-        if (sprite != null) {
-            sprite.setOrigin(origin);
+        if (graphicObject != null) {
+            graphicObject.setOrigin(origin);
         }
 
 //        if (this.physicsBody.collisionShape != null) {
@@ -53,12 +53,12 @@ public abstract class Actor {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Actor other)) return false;
-        return Objects.equals(origin, other.origin) && Objects.equals(physicsBody, other.physicsBody) && Objects.equals(sprite, other.sprite);
+        return Objects.equals(origin, other.origin) && Objects.equals(physicsBody, other.physicsBody) && Objects.equals(graphicObject, other.graphicObject);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(origin, physicsBody, sprite);
+        return Objects.hash(origin, physicsBody, graphicObject);
     }
 
     public void removeFromWorld() {
@@ -66,13 +66,13 @@ public abstract class Actor {
     }
 
     void setWorld(World world, GraphicsEngine graphicsEngine) {
-        graphicsEngine.add(sprite);
+        graphicsEngine.add(graphicObject);
         // TODO: Eventually include PhysicsEngine.add(physicsBody)
         this.world = world;
     }
 
     void destroy() {
-        sprite.removeFromParent();
+        graphicObject.removeFromParent();
         // TODO: Eventually include physicsBody.removeFromSystem()
         world = null;
     }
