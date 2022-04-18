961143313585dbe1adff2d1a61b0f74ae55007a3
Tessa Power <tessa@tessapower.co>
2022-04-18 19:54:01 +1200

    Implement AnimatedSprite (mlem!)

--- graphics/SpriteSequence.java
+++ graphics/SpriteSequence.java
@@ -8,4 +8,8 @@ public record SpriteSequence(List<GridSquare> frames, boolean loops) {
     public int numFrames() {
         return frames.size();
     }
+
+    public int lastFrame() {
+        return numFrames() - 1;
+    }
 }
