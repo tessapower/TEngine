9cc0cad25a3b5fb9a24d1204b580aeea200e4378
Tessa Power <tessa@tessapower.co>
2022-05-09 16:25:35 +1200

    Ensure Vector has a default constructor

--- src/main/java/tengine/physics/kinematics/Vector.java
+++ src/main/java/tengine/physics/kinematics/Vector.java
@@ -1,6 +1,10 @@
 package tengine.physics.kinematics;
 
 public record Vector(double x, double y) {
+    public Vector() {
+        this(0, 0);
+    }
+
     public double magnitude() {
         return Math.sqrt(x * x + y * y);
     }
--- src/main/java/tengine/physics/kinematics/Velocity.java
+++ src/main/java/tengine/physics/kinematics/Velocity.java
@@ -1,3 +1,7 @@
 package tengine.physics.kinematics;
 
-public record Velocity(double speed, Vector direction) {}
+public record Velocity(double speed, Vector direction) {
+    public Velocity() {
+        this(0, new Vector(1, 0));
+    }
+}
