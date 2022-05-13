ffd01f8c89c48a4852a7f6a73cafe385a91757ba
Tessa Power <tessa@tessapower.co>
2022-05-13 15:26:00 +1200

    Convert Velocity from Record to Class

--- src/main/java/tengine/physics/kinematics/Velocity.java
+++ src/main/java/tengine/physics/kinematics/Velocity.java
@@ -1,7 +1,35 @@
 package tengine.physics.kinematics;
 
-public record Velocity(double speed, Vector direction) {
+public class Velocity {
+    double speed = 0;
+    Vector direction;
+
     public Velocity() {
-        this(0, new Vector(1, 0));
+        this(0, new Vector(0, 0));
+    }
+
+    public Velocity (double speed, Vector direction) {
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    public void invertDirection() {
+        direction = new Vector(-1 * direction.x(), -1 * direction.y());
+    }
+
+    public void invertXDirection() {
+        direction = new Vector(-1 * direction.x(), direction.y());
+    }
+
+    public void invertYDirection() {
+        direction = new Vector(direction.x(), -1 * direction.y());
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDirection(Vector direction) {
+        this.direction = direction;
     }
 }
