9a853268915e6f3d312b4119ecc96ae31191b256
Tessa Power <tessa@tessapower.co>
2022-05-15 19:00:18 +1200

    Don't track physics components of actors on the world

--- src/main/java/tengine/world/World.java
+++ src/main/java/tengine/world/World.java
@@ -2,16 +2,13 @@ package tengine.world;
 
 import tengine.Actor;
 import tengine.graphics.components.TGraphicCompound;
-import tengine.physics.TPhysicsComponent;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class World {
     protected TGraphicCompound canvas;
-    protected List<TPhysicsComponent> physicsComponents = new ArrayList<>();
     protected List<Actor> actors = new ArrayList<>();
 
     public World(Dimension dimension) {
@@ -22,7 +19,6 @@ public class World {
         actor.addToWorld(this);
         actors.add(actor);
         canvas.add(actor.graphic());
-        physicsComponents.add(actor.physics());
     }
 
     public void add(Actor... actors) {
@@ -37,16 +33,11 @@ public class World {
 
     public void remove(Actor actor) {
         actors.remove(actor);
-        physicsComponents.remove(actor.physics());
         actor.destroy();
     }
 
     public TGraphicCompound canvas() {
         return canvas;
-    }
-
-    public List<TPhysicsComponent> physicsComponents() {
-        return Collections.unmodifiableList(physicsComponents);
     }
 
     public List<Actor> actors() {
