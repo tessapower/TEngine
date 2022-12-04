f6dc54e18313cd652dd75a65c3e1917c608df804
Tessa Power <tessa@tessapower.co>
2022-12-04 17:58:46 +1300

    Uncomment NarrowPhaseCollisionDetector test placeholders and TODOs

--- src/test/java/physics/collisions/NarrowPhaseDetectorTests.java
+++ src/test/java/physics/collisions/NarrowPhaseDetectorTests.java
@@ -1,35 +1,24 @@
-//package physics.collisions;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import tengine.physics.TPhysicsComponent;
-//import tengine.physics.collisions.detection.NarrowPhaseDetector;
-//import tengine.physics.collisions.events.CollisionEvent;
-//import tengine.physics.collisions.shapes.CollisionRect;
-//import tengine.physics.kinematics.TVector;
-//import tengine.physics.kinematics.TVelocity;
-//
-//import java.awt.*;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-///**
-// * <code>NarrowPhaseDetector</code> tests involve testing the <code>CollisionEvent</code> that is produced by two
-// * given <code>TPhysicsComponent</code>s, one that is moving and one that is static. We expect that the impact vector
-// * indicates the path the moving <code>TPhysicsComponent</code> took up until colliding with the static
-// * <code>TPhysicsComponent</code>, and that the impact side indicates the side of the moving <code>TPhysicsComponent</code>
-// * collided with the static <code>TPhysicsComponent</code>.
-// * <br><br>
-// * <img src="doc-files/impact_side.png" />
-// */
-//class NarrowPhaseDetectorTests {
-//
-//    /**
-//     * <img src="doc-files/collision_rect_rect_+x_0y.png" />
-//     */
-//    @Test
-//    @DisplayName("Rect vs. Rect (+, 0)")
-//    void rectRect_PosX_ZeroY() {
+package physics.collisions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <code>NarrowPhaseDetector</code> tests involve testing the <code>CollisionEvent</code> that is produced by two
+ * given <code>TPhysicsComponent</code>s, one that is moving and one that is static. We expect that the impact vector
+ * indicates the path the moving <code>TPhysicsComponent</code> took up until colliding with the static
+ * <code>TPhysicsComponent</code>, and that the impact side indicates the side of the moving <code>TPhysicsComponent</code>
+ * collided with the static <code>TPhysicsComponent</code>.
+ * <br><br>
+ * <img src="doc-files/impact_side.png" />
+ */
+class NarrowPhaseDetectorTests {
+    /**
+     * <img src="doc-files/collision_rect_rect_+x_0y.png" />
+     */
+    @Test
+    @DisplayName("Rect vs. Rect (+, 0)")
+    void rectRect_PosX_ZeroY() {
 //        boolean isStatic = true;
 //        boolean hasCollisions = true;
 //
@@ -59,39 +48,40 @@
 //
 //        CollisionEvent actual = NarrowPhaseDetector.detect(movingBody, staticBody);
 //        assertEquals(actual, expected);
-//    }
-//
-//    /**
-//     * <img src="doc-files/collision_rect_rect_-x_0y.png" />
-//     */
-//    @Test
-//    @DisplayName("Rect vs. Rect (-, 0)")
-//    void rectRect_NegX_ZeroY()  {
-//
-//    }
-//
-//    // pure y velocity
-//        // positive y
-//        // negative y
-//
-//    // x == y -> 45 degree angles
-//
-//    // +x, +y
-//        //
-//
-//    // +x, -y
-//
-//    // -x, +y
-//
-//    // -x, -y
-//
-//    /**
-//     * <img src="doc-files/collision_rect_rect_-x_-y_right.png" />
-//     */
-//    @Test
-//    @DisplayName("Rect vs. Rect (-, -) Impact Right")
-//    void rectRect_NegX_NegY_Right() {
-//
-//    }
-//
-//}
+    }
+
+    /**
+     * <img src="doc-files/collision_rect_rect_-x_0y.png" />
+     */
+    @Test
+    @DisplayName("Rect vs. Rect (-, 0)")
+    void rectRect_NegX_ZeroY()  {
+        // TODO
+    }
+
+    /**
+     * <img src="doc-files/collision_rect_rect_-x_-y_right.png" />
+     */
+    @Test
+    @DisplayName("Rect vs. Rect (-, -) Impact Right")
+    void rectRect_NegX_NegY_Right() {
+        // TODO
+    }
+
+    // TODO: Implement the following tests
+    // pure y velocity
+    // positive y
+    // negative y
+
+    // x == y -> 45 degree angles
+
+    // +x, +y
+    //
+
+    // +x, -y
+
+    // -x, +y
+
+    // -x, -y
+
+}
