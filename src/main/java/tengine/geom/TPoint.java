979e666ce8c872e3e0c45400b0183df2f8e4288d
Tessa Power <tessa@tessapower.co>
2022-12-04 17:45:13 +1300

    Add nice comments to TPoint

--- src/main/java/tengine/geom/TPoint.java
+++ src/main/java/tengine/geom/TPoint.java
@@ -2,6 +2,9 @@ package tengine.geom;
 
 import java.awt.geom.Point2D;
 
+/**
+ * Defines a point representing a location in (x, y) coordinate space.
+ */
 public class TPoint extends Point2D.Double {
     public TPoint(double x, double y) {
         super(x, y);
@@ -11,6 +14,9 @@ public class TPoint extends Point2D.Double {
         super();
     }
 
+    /**
+     * Returns a new TPoint which is this TPoint translated by the given dx and dy.
+     */
     public TPoint translate(double dx, double dy) {
         return new TPoint(this.x + dx, this.y + dy);
     }
